406248bd6e6d66ed0bf1cda94b2bc9fbd62084bf
Afnan_Rafid <70755811+Afnan-Rafid@users.noreply.github.com>
2026-04-30 20:41:41 +0600

    remove sensitive data

--- send_mail.php
+++ send_mail.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -7,33 +8,36 @@ require 'PHPMailer/PHPMailer-master/src/Exception.php';
 require 'PHPMailer/PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer/PHPMailer-master/src/SMTP.php';
 
-function sendMail($email,$subject,$message){
+function sendMail($email, $subject, $message){
+    $env = parse_ini_file(__DIR__ . '/.env');
 
-$mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
 
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = getenv("SMTP_EMAIL");
-$mail->Password = getenv("SMTP_PASSWORD");
-$mail->SMTPSecure = 'tls';
-$mail->Port = 587;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
 
-$mail->SMTPOptions = array(
-    'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-    )
-);
+    $mail->Username = $env['SMTP_EMAIL'];
+    $mail->Password = $env['SMTP_PASSWORD'];
 
-$mail->setFrom($env['SMTP_EMAIL'], 'Afnan Rafid');
-$mail->addAddress($email);
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-$mail->isHTML(true);
-$mail->Subject = $subject;
-$mail->Body = $message;
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
 
-$mail->send();
+    $mail->setFrom($env['SMTP_EMAIL'], 'Afnan Rafid');
 
+    $mail->addAddress($email);
+
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+
+    $mail->send();
 }
